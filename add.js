47d41cf8e3d1e44340e5aa2e3775b5e7c188c554
IvanPerez9450 <77861317+IvanPerez9450@users.noreply.github.com>
2021-02-19 08:52:15 -0600

    Update add.js

--- add.js
+++ add.js
@@ -1,173 +1,189 @@
+///////////////////////////////////////// * JS FUNDAMENTALS 1 * //////////////////////////////////////////////////
 
-////////////////////////////// * Basics
+* Basics
+* let & const
+* Basics Operators (const, let, concatenations)
+* Coding Challenge 1
+* Strings, Template Literals, Multipline 
+* if & else Statements
+* Coding Challenge 2
+* Conversion and Coercion
+* Equality Operators == vs ===
+* Coding Challenge 3
+* The switch Statement
+* Statement and Expressions
+* Conditional (Ternary) Operator
+* Coding Challenge 4
 
-// console.log("Ivan");
-// console.log(23);
+///////////////////////////////////////////////// * Basics * /////////////////////////////////////////////////////
 
-// let firstName = "Ivan";
-// console.log(firstName);
+console.log("Ivan");
+console.log(23);
 
-// let country = "Cuba";
-// let continent = "Americas";
-// let population = "601 million";
+let firstName = "Ivan";
+console.log(firstName);
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+let country = "Cuba";
+let continent = "Americas";
+let population = "601 million";
 
-// true;
-// console.log(true)
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// let ImRockingJS = true;            // boolean
-// console.log(ImRockingJS);
+true;
+console.log(true)
 
-// console.log(typeof true);
-// console.log(typeof 33);
-// console.log(typeof "Ivan");
+let ImRockingJS = true;            // boolean
+console.log(ImRockingJS);
 
-////////////////////////////////// * let & const
+console.log(typeof true);
+console.log(typeof 33);
+console.log(typeof "Ivan");
 
-// let age = 38;
-// age = 40;
-// console.log(age);
+///////////////////////////////////////////////////////////////////////////// * let & const
 
-// const birthYears = 1991;
-// console.log(birthYears);
+let age = 38;
+age = 40;
+console.log(age);
 
-////////////////////////////////// * Basics Operators
+const birthYears = 1991;
+console.log(birthYears);
 
-// const now = 2030;
-// const ageIvan = now - 1988;
-// const ageRocky = now - 2017;
+////////////////////////////////////////////////////////////////////////// * Basics Operators
 
-// console.log(ageIvan, ageRocky);
-// console.log(ageIvan * 2, ageIvan / 2, 2 ** 3);
+const now = 2030;
+const ageIvan = now - 1988;
+const ageRocky = now - 2017;
 
-// const firstName = 'Ivan';
-// const lastName = 'Perez';
-// console.log(firstName + ' ' + lastName);         //concatenations
+console.log(ageIvan, ageRocky);
+console.log(ageIvan * 2, ageIvan / 2, 2 ** 3);
 
-// let x = 10 + 5;
-// x += 10;
-// x ++;
-// x --;
-// console.log(x);
+const firstName = 'Ivan';
+const lastName = 'Perez';
+console.log(firstName + ' ' + lastName);         //concatenations
 
-// console.log(ageIvan < ageRocky)
-// console.log(ageRocky >= 10);
+let x = 10 + 5;
+x += 10;
+x ++;
+x --;
+console.log(x);
 
-// const now = 2030;
-// const ageIvan = now - 1988;
-// const ageRocky = now - 2017;
+console.log(ageIvan < ageRocky)
+console.log(ageRocky >= 10);
 
-// console.log(now - 1988 > now - 2017);
-// let x, y;
-// x = y = 25 - 10 - 5;
-// console.log(x, y);
+const now = 2030;
+const ageIvan = now - 1988;
+const ageRocky = now - 2017;
 
-// const aveAge = (ageIvan + ageRocky) / 2
-// console.log(aveAge);
+console.log(now - 1988 > now - 2017);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
 
-//////////////////////////////////// * Coding Challenge 1
+const aveAge = (ageIvan + ageRocky) / 2
+console.log(aveAge);
 
-// const markWeight = 78;     
-// const markHeigth = 1.69;
-// const johnWeight = 92; 
-// const johnHeigth = 1.95;
+/////////////////////////////////////////////////////////////////// * Coding Challenge 1
 
-// const markBmi = markWeight / (markHeigth ** 2); 
-// const johnBmi = johnWeight / (johnHeigth ** 2); 
+const markWeight = 78;     
+const markHeigth = 1.69;
+const johnWeight = 92; 
+const johnHeigth = 1.95;
 
-// console.log(markBmi, johnBmi);
+const markBmi = markWeight / (markHeigth ** 2); 
+const johnBmi = johnWeight / (johnHeigth ** 2); 
 
-// const markHigherBMI = markBmi > johnBmi;
-// console.log(markHigherBMI)
+console.log(markBmi, johnBmi);
 
-
-// const markWeight2 = 95;     
-// const markHeigth2 = 1.88;
-// const johnWeight2 = 85; 
-// const johnHeigth2 = 1.76;
-
-// const markBmi2 = markWeight2 / (markHeigth2 ** 2); 
-// const johnBmi2 = johnWeight2 / (johnHeigth2 ** 2); 
-
-// console.log(markBmi2, johnBmi2);
-
-// const markHigherBMI2 = markBmi2 > johnBmi2;
-// console.log(markHigherBMI2)
-
-/////////////////////////////////// * Strings, Template Literals, Multipline 
-
-// const firstName = 'Ivan';
-// const job = 'coder';
-// const birthYears = '1988';
-// const year = 2030;
-
-// console.log( "I'm" + ' ' + firstName + ', a' + ' ' + (year - birthYears)+ ' ' + 'years old'+ ' ' + job + '!');
-
-// const ivanNew = `I'm ${firstName}, a ${year - birthYears} year old ${job} !`;
-
-// console.log(ivanNew)
-
-// console.log(`String
-// multiple
-// lines`);
-
-///////////////////////////////// * if & else
-
-// const age = 15;
-
-// if (age >= 18) {
-//     console.log('Sarah can take the test');
-// } else {
-//     const yearsLeft = 18 - age;
-//     console.log(`Sarah is too young to drive, wait ${yearsLeft} more years`);
-// }
-
-// const birthYears = 2012;
-// let century;
-
-// if (birthYears <= 2000){
-//     century = 20;
-// } else {
-//     century = 21;
-// }
-// console.log(century)
-
-///////////////////////////////// * Coding Challenge 2
-
-// const markWeight = 78;     
-// const markHeigth = 1.69;
-// const johnWeight = 92; 
-// const johnHeigth = 1.95;
-
-// const markBmi = markWeight / (markHeigth ** 2); 
-// const johnBmi = johnWeight / (johnHeigth ** 2); 
-
-// console.log(`Mark's BMI ${markBmi}`);
-// console.log(`John's BMI ${johnBmi}`);
+const markHigherBMI = markBmi > johnBmi;
+console.log(markHigherBMI)
 
 
-// const markHigherBMI = markBmi > johnBmi;
-// console.log(markHigherBMI)
+const markWeight2 = 95;     
+const markHeigth2 = 1.88;
+const johnWeight2 = 85; 
+const johnHeigth2 = 1.76;
 
-// if (markBmi > johnBmi) {
-//     console.log(`Mark's BMI ${markBmi} is higher that John's ${johnBmi}`)
-// }  else {
-//     console.log(`Mark's BMI ${johnBmi} is higher that John's ${markBmi}`)
-// }
+const markBmi2 = markWeight2 / (markHeigth2 ** 2); 
+const johnBmi2 = johnWeight2 / (johnHeigth2 ** 2); 
 
-///////////////////////////////// * Conversion and Coercion
+console.log(markBmi2, johnBmi2);
 
-// const inputYear = '1991';                   
+const markHigherBMI2 = markBmi2 > johnBmi2;
+console.log(markHigherBMI2)
 
-// console.log( '1991' + 18);                  // String
-// console.log(Number(inputYear) + 18 );       // converting to Number
-// console.log(String(23), 23);                // Converting to String
+//////////////////////////////////////////////////////////// * Strings, Template Literals, Multipline 
 
-///////////////////////////////// * Truthy and Falsy Values
-// 5 falsy values: 0, '', undefined, null, NaN
+const firstName = 'Ivan';
+const job = 'coder';
+const birthYears = '1988';
+const year = 2030;
+
+console.log( "I'm" + ' ' + firstName + ', a' + ' ' + (year - birthYears)+ ' ' + 'years old'+ ' ' + job + '!');
+
+const ivanNew = `I'm ${firstName}, a ${year - birthYears} year old ${job} !`;
+
+console.log(ivanNew)
+
+console.log(`String
+multiple
+lines`);
+
+////////////////////////////////////////////////////////////////////////// * if & else
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can take the test');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young to drive, wait ${yearsLeft} more years`);
+}
+
+const birthYears = 2012;
+let century;
+
+if (birthYears <= 2000){
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century)
+
+////////////////////////////////////////////////////////////// * Coding Challenge 2
+
+const markWeight = 78;     
+const markHeigth = 1.69;
+const johnWeight = 92; 
+const johnHeigth = 1.95;
+
+const markBmi = markWeight / (markHeigth ** 2); 
+const johnBmi = johnWeight / (johnHeigth ** 2); 
+
+console.log(`Mark's BMI ${markBmi}`);
+console.log(`John's BMI ${johnBmi}`);
+
+
+const markHigherBMI = markBmi > johnBmi;
+console.log(markHigherBMI)
+
+if (markBmi > johnBmi) {
+    console.log(`Mark's BMI ${markBmi} is higher that John's ${johnBmi}`)
+}  else {
+    console.log(`Mark's BMI ${johnBmi} is higher that John's ${markBmi}`)
+}
+
+//////////////////////////////////////////////////////////////////// * Conversion and Coercion
+
+const inputYear = '1991';                   
+
+console.log( '1991' + 18);                  // String
+console.log(Number(inputYear) + 18 );       // converting to Number
+console.log(String(23), 23);                // Converting to String
+
+/////////////////////////////// * Truthy and Falsy Values
+5 falsy values: 0, '', undefined, null, NaN
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean(''));
@@ -182,4 +198,158 @@ if (money) {
     console.log('You should get a job');
 }
 
-///////////////////////////////// * Equality Operators == vs ===
+//////////////////////////////////////////////////////////////////// * Equality Operators == vs ===
+
+const age = 18;
+if (age === 18) console.log(`you just became and adult`);
+
+//////////////////////////////////////////// * Boolen Logic and Logocal Operators (and && , or ||, not !)
+
+const hasDriverLicense = true;  // A
+const hasGoodVison = false;      // B
+
+console.log(hasDriverLicense && hasGoodVison);     // && and
+console.log(hasDriverLicense || hasGoodVison);     // || or
+console.log(!hasDriverLicense);                    // ! not
+
+if (hasDriverLicense && hasGoodVison) {
+    console.log(`Sarah is able to drive`);
+} else {
+    console.log (`Someone else should drive`);
+}
+
+const hasDriverLicense = true;    // A
+const hasGoodVison = true;        // B
+const isTired = false;            // C
+console.log(hasDriverLicense || hasGoodVison || isTired);
+console.log(hasDriverLicense && hasGoodVison || isTired);
+
+
+if (hasDriverLicense && hasGoodVison && !isTired) {
+    console.log(`Sarah is able to drive`);
+} else {
+    console.log (`Someone else should drive`);
+}
+
+////////////////////////////////////////////////////////////// * Coding Challenge 3
+
+const fistGameDolphins = (96 + 108 + 89) / 3;
+const fistGameKoalas = (88 + 91 + 110) / 3;
+
+const secondGameDolphins = (97 + 112 + 101) / 3;
+const secondGameKoalas = (109 + 95 + 123) / 3;
+
+const thirdGameDolphins = (97 + 112 + 101) / 3;
+const thirdGameKoalas = (109 + 95 + 106) / 3;
+
+const aveScoreDolphins = (fistGameDolphins + secondGameDolphins + thirdGameDolphins) / 3;
+const aveScoreKoalas = (fistGameKoalas + secondGameKoalas + thirdGameKoalas) / 3;
+
+console.log(`aveScoreDolphins ${aveScoreDolphins}`);
+console.log(`aveScoreKoalas ${aveScoreKoalas}`);
+
+if (aveScoreKoalas && aveScoreDolphins < 100) {
+    console.log(`Scrore under 100, The is not winner`)
+} else if (aveScoreKoalas > aveScoreDolphins) {
+    console.log(`Koalas team is the winner`)
+} else if (aveScoreKoalas === aveScoreDolphins) {
+    console.log(`The was not winner, game is a draw`)
+} else {
+    console.log(`Dolphins team is the winner`)
+}
+
+////////////////////////////////////////////////////////////// * The switch Statement
+
+const day = 'monday';
+
+switch(day) {
+    case 'monday':
+        console.log(`Plan course Structure`);
+        console.log(`Go coding Meetup`);
+        break;
+    case 'tuesday':
+        console.log(`Prepare Theory Videos`);
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log(`Write Code examples`);
+        break;
+    case 'friday':
+        console.log(`Record Videos`);
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log (`Enjoy weekend`);
+        break;
+    default:
+        console.log(`Not a valid day`);
+}
+
+// (and && , or ||, not !)
+
+
+
+if (day === 'monday') {
+    console.log('Plan course Structure');
+    console.log('Go coding Meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare Theory Videos');
+} else if ( day === 'wednesday' || day === 'thursday') {
+    console.log('Write Code examples');
+} else if ( day === 'friday') {
+    console.log('Record Videos');
+} else if ( day === 'saturday' || day === 'sunday') {
+    console.log ('Enjoy weekend')
+} else {
+    console.log('Not a valid day')
+}
+
+////////////////////////////////////////////////////////////// * Statement and Expressions
+
+// Expressions
+3 * 4
+1991
+true && false && false
+
+// Statement
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+////////////////////////////////////////////////////////////////// * Conditional (Ternary) Operator
+
+age >= 18 ? console.log ('I like to drink wine'):   //age >= 18(greater or equal 18) ? if the condition is true, execute the code ('I like to drink wine')
+console.log ('I like to drink water');
+
+const age = 10;
+const drink = age >= 18 ? 'wine' : 'water';
+
+console.log(drink);
+console.log(`I like to drink ${drink}`);
+
+////////////////////////////////////////////////////////////// * Coding Challenge 4
+
+SOLUTION 1
+const tip = 40;
+
+const largerBill = 430;           
+const largerBillTip = 430 * (20/100);
+const largerBillTotal = largerBill + largerBillTip;
+const middleBill = 275;           
+const middleBillTip = 275 * (15/100);  
+const middleBillTotal = middleBill + middleBillTip;          
+const smallerBill = 40;            
+const smallerBillTip = 40 * (20/100);
+const smallerBillTotal = smallerBill + smallerBillTip; 
+
+tip === 40 ? console.log (`The bill was ${smallerBill}, the tip was ${smallerBillTip}, and the total value ${smallerBillTotal}`):
+tip === 275 ? console.log (`The bill was ${middleBill}, the tip was ${middleBillTip}, and the total value ${middleBillTotal}`):
+tip === 430 ? console.log (`The bill was ${largerBill}, the tip was ${largerBillTip}, and the total value ${largerBillTotal}`):
+console.log ('I like to drink water');
+
+
+SOLUTION 2
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log (`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
